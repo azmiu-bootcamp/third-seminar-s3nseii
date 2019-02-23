5882677d530e1d9940f3b9b53aa4255e0446f79f
s3nseii <47414629+s3nseii@users.noreply.github.com>
2019-02-23 22:11:00 +0400

    Create problem 1 Solved.py

---
+++ problem 1 Solved.py
@@ -0,0 +1,21 @@
+#max value of matrix
+n = 5
+end = 0
+matrix = []
+
+
+while n != 0:
+
+    n = int(input('Matrise daxil etmek istediyiniz ededleri girin: '))
+
+    matrix.append(n)
+#this is just warning
+    if len(matrix) > 3:
+        print ('bitirmek istediyiniz zaman 0 daxil edin')
+
+    if len(matrix) > 6:
+        print('bitirmek istediyiniz zaman 0 daxil edin')
+
+
+print(matrix)
+print (max(matrix))
